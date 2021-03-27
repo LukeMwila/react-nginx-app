438052c544ba2b010911f52b38e1d6a231113aa6
Luke Mwila <outlier.developer@gmail.com>
2021-03-27 14:14:46 +0200

    refactor: updated display text

--- src/App.js
+++ src/App.js
@@ -2,34 +2,10 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const [toDoItems, updateToDoItems] = React.useState([]);
-
-  React.useEffect(() => {
-    const getToDoItems = async () => {
-      const response = await fetch(
-        `/backend/v1/to-do`
-      );
-
-      const items = await response.json();
-      if (items && Array.isArray(items) && items.length) {
-        updateToDoItems(items);
-      }
-    };
-    getToDoItems();
-  }, []);
 
   return (
     <div>
-      {toDoItems && toDoItems.length
-        ? toDoItems.map((item, i) => {
-            return (
-              <div key={i}>
-                {`${item.item_name}`}
-                <br />
-              </div>
-            );
-          })
-        : 'No items to be done'}
+      Show me the update
     </div>
   );
 }
